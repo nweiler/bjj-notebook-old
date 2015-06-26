f0b3628ba061f1429edbbc9404c2bbecbd90cbfa
Nick Weiler <nick.weiler@aunalytics.com>
2015-06-25 22:39:30 -0400

    switched to jade

--- index.js
+++ index.js
@@ -12,21 +12,15 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 app.use('/', router);
 
-mongoose.connect('mongodb://mongo_user:user_mongo@ds043062.mongolab.com:43062/heroku_934vsv2t');
-var db = mongoose.connection;
 
-db.once('open', function() {
-
-  router.get('/', function(req, res) {
-    api.get_latest(req, res, db, function(e, d) {
-      if (e) return console.log(error);
-      res.send(JSON.stringify(d));
-    })
-  })
-
-  app.listen(app.get('port'), function() {
-    console.log('Server running at http://localhost:' + app.get('port'));
+router.get('/', function(req, res) {
+  api.get_latest(req, res, function(e, d) {
+    if (e) return console.log(error);
+    res.render('index', { data: JSON.stringify(d) });
   })
 })
 
+app.listen(app.get('port'), function() {
+  console.log('Server running at http://localhost:' + app.get('port'));
+})
 
